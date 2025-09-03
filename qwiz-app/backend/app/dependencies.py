d0ac021f18fb8393ec611d0a3dac6f11dfba5232
Tylah Jenkins <tylahjenkins@icloud.com>
2025-09-04 08:14:11 +1000

    task: improving backend wit dependencies, data available and saved, analytics setup

--- qwiz-app/backend/app/dependencies.py
+++ qwiz-app/backend/app/dependencies.py
@@ -2,7 +2,9 @@
 from google.cloud import firestore
 from app.config import settings
 from app.services import SessionManager
+from app.analytics import AnalyticsService
 
 # Initialize the Firestore client and SessionManager instance here
 db = firestore.Client(project=settings.GOOGLE_CLOUD_PROJECT)
 session_manager = SessionManager(db_client=db)
+analytics_service = AnalyticsService(db_client=db, session_manager=session_manager)
